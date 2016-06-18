ebb30fdda46effce84c8e4d9e3890e53d0fcf9b7
dengjiayao <dnxbf321@gmail.com>
2016-06-18 10:43:40 +0800

    `npm run build` come error if `tasks/lib` is ignore

---
+++ test-workspace/tasks/lib/util.js
@@ -0,0 +1,67 @@
+/**
+ * @author Toru Nagashima
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict";
+
+/*eslint no-var:"off"*/
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var fs = require("fs");
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+var FILE_NAME = "test.txt";
+
+//------------------------------------------------------------------------------
+// Public Interface
+//------------------------------------------------------------------------------
+
+/**
+ * Gets the result text from `test.txt`.
+ *
+ * @returns {string|null} The result text.
+ */
+exports.result = function result() {
+    try {
+        return fs.readFileSync(FILE_NAME, {encoding: "utf8"});
+    }
+    catch (err) {
+        if (err.message.indexOf("ENOENT") < 0) {
+            console.error("ERROR:", err.stack);
+        }
+        return null;
+    }
+};
+
+/**
+ * Appends text to `test.txt`.
+ *
+ * @param {string} content - A text to append.
+ * @returns {void}
+ */
+exports.appendResult = function appendResult(content) {
+    fs.appendFileSync(FILE_NAME, content);
+};
+
+/**
+ * Removes `test.txt`.
+ *
+ * @returns {void}
+ */
+exports.removeResult = function removeResult() {
+    try {
+        fs.unlinkSync(FILE_NAME);
+    }
+    catch (err) {
+        if (err.message.indexOf("ENOENT") < 0) {
+            console.error("ERROR:", err.stack);
+        }
+    }
+};
