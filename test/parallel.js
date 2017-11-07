21c02697e59748c0d2ac55b5244bd0ca10ecbfcc
Toru Nagashima <star.ctor@gmail.com>
2017-11-07 21:00:25 +0900

    Chore: trivial fix

--- test/parallel.js
+++ test/parallel.js
@@ -240,37 +240,37 @@ describe("[parallel]", () => {
     describe("should abort other tasks when a task finished, when --race option was specified:", () => {
         it("Node API", async () => {
             await nodeApi(["test-task:append1 a", "test-task:append2 b"], { parallel: true, race: true })
-            await delay(1000)
+            await delay(5000)
             assert(result() === "a" || result() === "ab" || result() === "ba")
         })
 
         it("npm-run-all command (--race)", async () => {
             await runAll(["--race", "--parallel", "test-task:append1 a", "test-task:append2 b"])
-            await delay(1000)
+            await delay(5000)
             assert(result() === "a" || result() === "ab" || result() === "ba")
         })
 
         it("npm-run-all command (-r)", async () => {
             await runAll(["-rp", "test-task:append1 a", "test-task:append2 b"])
-            await delay(1000)
+            await delay(5000)
             assert(result() === "a" || result() === "ab" || result() === "ba")
         })
 
         it("run-p command (--race)", async () => {
             await runPar(["--race", "test-task:append1 a", "test-task:append2 b"])
-            await delay(1000)
+            await delay(5000)
             assert(result() === "a" || result() === "ab" || result() === "ba")
         })
 
         it("run-p command (-r)", async () => {
             await runPar(["-r", "test-task:append1 a", "test-task:append2 b"])
-            await delay(1000)
+            await delay(5000)
             assert(result() === "a" || result() === "ab" || result() === "ba")
         })
 
         it("run-p command (no -r)", async () => {
             await runPar(["test-task:append1 a", "test-task:append2 b"])
-            await delay(1000)
+            await delay(5000)
             assert(result() === "abb" || result() === "bab")
         })
     })
