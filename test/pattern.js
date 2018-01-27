12504fdf4331c414b77e9c9c1333d832201a910c
Erik Kemperman <erikkemperman@gmail.com>
2018-01-27 17:27:09 +0100

    Add failing test with exclamation mark

--- test/pattern.js
+++ test/pattern.js
@@ -143,4 +143,49 @@ describe("[pattern] it should run matched tasks if glob like patterns are given.
             }
         })
     })
+
+    describe("\"!test-task:**\" should not match to anything", () => {
+        it("Node API", async () => {
+            try {
+                await nodeApi("!test-task:**")
+                assert(false, "should not match")
+            }
+            catch (err) {
+                assert((/not found/i).test(err.message))
+            }
+        })
+
+        it("npm-run-all command", async () => {
+            const stderr = new BufferStream()
+            try {
+                await runAll(["!test-task:**"], null, stderr)
+                assert(false, "should not match")
+            }
+            catch (_err) {
+                assert((/not found/i).test(stderr.value))
+            }
+        })
+
+        it("run-s command", async () => {
+            const stderr = new BufferStream()
+            try {
+                await runSeq(["!test-task:**"], null, stderr)
+                assert(false, "should not match")
+            }
+            catch (_err) {
+                assert((/not found/i).test(stderr.value))
+            }
+        })
+
+        it("run-p command", async () => {
+            const stderr = new BufferStream()
+            try {
+                await runPar(["!test-task:**"], null, stderr)
+                assert(false, "should not match")
+            }
+            catch (_err) {
+                assert((/not found/i).test(stderr.value))
+            }
+        })
+    })
 })
